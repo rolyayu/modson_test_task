5fb08fd4b798d51c122863a7c5d6a1b0a36d0021
Anton Rudkosvky <ranton497@gmail.com>
2023-04-05 19:58:49 +0300

    Create function that configure process.env property

---
+++ src/utils/dotenv.config.ts
@@ -0,0 +1,12 @@
+import * as dotenv from 'dotenv';
+
+/**
+ * Configurate process.env according to NODE_ENV attribute(given via cross-env NODE_ENV=<type>)
+ */
+const configEnv = () => {
+    dotenv.config({
+        path: `.${process.env.NODE_ENV}.env`
+    })
+}
+
+export default configEnv;
