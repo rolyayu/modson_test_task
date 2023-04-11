fb39823409607ba3ce99875fefd76b844b3e6539
Anton Rudkosvky <ranton497@gmail.com>
2023-04-11 17:26:03 +0300

    Change approach to interact with db

---
+++ src/database/index.ts
@@ -0,0 +1,1 @@
+export { TypeOrmConnection } from './typeorm.connection';
--- src/database/typeorm.connection.ts
+++ src/database/typeorm.connection.ts
@@ -1,24 +1,34 @@
-import { DataSource, createConnection } from "typeorm";
+import { DataSource } from "typeorm";
 
 import configEnv from "../utils/dotenv.config";
 
-export const getDataSource = async () => {
-    configEnv();
-    const entitiesPath = __dirname + '/../**/*.entity.ts';
-    const TypeOrmConnection = await new DataSource(
-        {
-            type: 'postgres',
-            host: process.env.PG_HOST,
-            port: Number(process.env.PG_PORT),
-            username: process.env.PG_USERNAME,
-            password: process.env.PG_PASSWORD,
-            database: process.env.PG_DATABASE,
-            entities: [entitiesPath],
-            logging: true,
-            synchronize: true
+export class TypeOrmConnection {
+    private static connection: DataSource;
+
+    private constructor() { }
+
+    public static getConnection = (): DataSource => {
+        if (!this.connection) {
+            this.connection = this.configureDataSource();
         }
-    ).initialize();
-    await TypeOrmConnection.synchronize()
-        .then(() => console.log('Connection established'));
-    return TypeOrmConnection;
+        return this.connection;
+    }
+
+    private static configureDataSource = (): DataSource => {
+        configEnv();
+        const entitiesPath = __dirname + '/../**/*.entity.ts';
+        return new DataSource(
+            {
+                type: 'postgres',
+                host: process.env.PG_HOST,
+                port: Number(process.env.PG_PORT),
+                username: process.env.PG_USERNAME,
+                password: process.env.PG_PASSWORD,
+                database: process.env.PG_DATABASE,
+                entities: [entitiesPath],
+                logging: true,
+                synchronize: true
+            }
+        )
+    }
 }
