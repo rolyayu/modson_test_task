7e3026ea60175c298c081cdc14c6c4c276a4f62a
Anton Rudkosvky <ranton497@gmail.com>
2023-04-17 13:26:41 +0300

    Change base service interface

--- src/interfaces/base-service.interface.ts
+++ src/interfaces/base-service.interface.ts
@@ -1,12 +1,9 @@
 import { FindOptionsWhere, ObjectLiteral } from "typeorm";
 
-export type QueryCriteria<T> = FindOptionsWhere<T>
 
 export interface IBaseService<T extends ObjectLiteral> {
     findById(id: number): Promise<T | null>;
     findAll(): Promise<T[]>;
-    findOneByCriteria(criteria: QueryCriteria<T>): Promise<T | null>;
-    findAllByCriteria(criteria: QueryCriteria<T>): Promise<T[] | null>;
 
     save(value: T): Promise<T>;
 
