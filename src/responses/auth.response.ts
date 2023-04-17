9ea397cc5f6c742a8e73e2d5bfdb7fe59f9df474
Anton Rudkosvky <ranton497@gmail.com>
2023-04-17 13:39:13 +0300

    Add authentication response

---
+++ src/responses/auth.response.ts
@@ -0,0 +1,6 @@
+
+export interface AuthorizationResponse {
+    accessToken: string;
+    refreshToken: string;
+    expiresIn: number;
+}
--- src/responses/index.ts
+++ src/responses/index.ts
@@ -1,2 +1,3 @@
 export { FailureResponse } from './failure.response';
 export { SuccessResponse } from './success.response';
+export { AuthorizationResponse } from './auth.response';
