cd2be4cd46c6d90e4eea47eca90063c989255db5
Anton Rudkosvky <ranton497@gmail.com>
2023-04-29 13:35:43 +0300

    add validation

--- src/meetups/dto/create.dto.ts
+++ src/meetups/dto/create.dto.ts
@@ -1,9 +1,24 @@
 
+import { IsArray, IsDate, IsOptional, Length, MaxLength, MinDate, MinLength, isDate, isNotEmpty, isString, } from 'class-validator';
+
 export class CreateMeetUpDto {
+    @MinLength(5, {
+        message: 'Title should contain at least 10 symbols.'
+    })
     readonly title: string;
+
+    @MinLength(20, {
+        message: 'Description should contain at least 20 symbols.'
+    })
     readonly description: string;
-    readonly tags?: string[];
-    readonly eventTime: Date = new Date();
+
+    @Length(3, 30, { each: true, message: 'Each tag should contains 3-30 sybmols.' })
+    @IsOptional()
+    readonly tags: string[] = [];
+
+    @IsDate()
+    @MinDate(new Date())
+    @IsOptional()
+    readonly eventTime: Date = new Date(Date.now() + 1000 * 3600 * 24);
 }
 
-export default CreateMeetUpDto;
