a62b419a5aed8677a8fa8dbc1bded7aba37793ea
Anton Rudkosvky <ranton497@gmail.com>
2023-04-29 19:49:16 +0300

    refactor: create barrel exports for errors

---
+++ src/errors/index.ts
@@ -0,0 +1,5 @@
+export { AuthError } from './AuthError';
+export { MeetUpNotFoundError } from './MeetUpNotFoundError';
+export { NotAllowedError } from './NotAllowedError';
+export { NotFoundError } from './NotFoundError';
+export { UserNotFoundError } from './UserNotFoundError';
