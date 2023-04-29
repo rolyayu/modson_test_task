0f042a0dc4f8290601572bafa4daf8821cc49624
Anton Rudkosvky <ranton497@gmail.com>
2023-04-29 20:21:21 +0300

    refactor: create barrel exports for meet up module

---
+++ src/meetups/index.ts
@@ -0,0 +1,4 @@
+export { MeetUpTag } from './meetups-tag.entity';
+export { MeetUp } from './meetups.entity';
+export { MeetUpFactory } from './meetups.factory';
+export { IMeetUpService } from './meetups.service.interface'
