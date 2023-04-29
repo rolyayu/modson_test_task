3408ab391a70d0a2917f03fc00841b6982761c99
Anton Rudkosvky <ranton497@gmail.com>
2023-04-29 20:00:07 +0300

    refactor: create barrel exports for users dto

---
+++ src/auth/users/dto/index.ts
@@ -0,0 +1,4 @@
+export { LoginUserDto } from './login.dto';
+export { RegisterUserDto } from './register.dto';
+export { ResponseUserDto } from './response.user.dto';
+export { UserMapper } from './users.mapper';
