4e34e1d77bf26a11d45143974a31ddcf6feb733d
Anton Rudkosvky <ranton497@gmail.com>
2023-04-29 20:11:34 +0300

    refactor: create barrel exports for meet ups dto

---
+++ src/meetups/dto/index.ts
@@ -0,0 +1,4 @@
+export { CreateMeetUpDto } from './create.dto';
+export { MeetUpDtoMapper } from './meetups.mapper';
+export { ResponseMeetUpDto } from './response.dto';
+export { UpdateMeetUpDto } from './update.dto';
