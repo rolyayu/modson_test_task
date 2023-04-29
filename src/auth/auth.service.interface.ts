37dfd32ca43b496d302f56911c6b843e65fe30e3
Anton Rudkosvky <ranton497@gmail.com>
2023-04-29 21:53:00 +0300

    refactor: update imports
    Update dto imports and remove unneeded imports in auth service interface

--- src/auth/auth.service.interface.ts
+++ src/auth/auth.service.interface.ts
@@ -1,9 +1,5 @@
 import { AuthorizationResponse } from "../responses";
-import { TokensPair } from "./jwt.service";
-import { LoginUserDto } from "./users/dto/login.dto";
-import { RegisterUserDto } from "./users/dto/register.dto";
-import { ResponseUserDto } from "./users/dto/response.user.dto";
-import { User } from "./users/users.entity";
+import { LoginUserDto, RegisterUserDto, ResponseUserDto } from "../users/dto";
 
 export interface IAuthService {
     register(register: RegisterUserDto): Promise<ResponseUserDto>;
