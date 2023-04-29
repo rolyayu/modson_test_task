a42e6dcb4a38197ae266e0c22f4f1ff171a15839
Anton Rudkosvky <ranton497@gmail.com>
2023-04-29 13:50:47 +0300

    export jwt related classes and interfaces

---
+++ src/auth/index.ts
@@ -0,0 +1,4 @@
+
+export { AccessTokenPayload as AccessToken } from './tokens/access.token'
+export { RefreshTokenPayload as RefreshToken } from './tokens/refresh.token'
+export { JwtService } from './jwt.service';
