10667c51dc896f7e612a12b18eb3a87f3ad3ef06
Anton Rudkosvky <ranton497@gmail.com>
2023-04-29 13:40:42 +0300

    add additional methods. Destructorization

--- src/meetups/dto/meetups.mapper.ts
+++ src/meetups/dto/meetups.mapper.ts
@@ -1,18 +1,44 @@
 import MeetUpTag from "../meetups-tag.entity";
 import MeetUp from "../meetups.entity";
-import CreateMeetUpDto from "./create.dto";
+import { CreateMeetUpDto } from "./create.dto";
+import { ResponseMeetUpDto } from "./response.dto";
+import { UpdateMeetUpDto } from "./update.dto";
 
 export class MeetUpDtoMapper {
-    static mapCreateMeetUpDto = (dto: CreateMeetUpDto): MeetUp => {
+    static mapCreateMeetUpDto = ({ title, description, eventTime, tags }: CreateMeetUpDto): MeetUp => {
         const meetUp = new MeetUp();
-        meetUp.title = dto.title;
-        meetUp.description = dto.description;
-        meetUp.eventTime = dto.eventTime;
-        meetUp.tags = dto.tags?.map(tagName => {
+        meetUp.title = title;
+        meetUp.description = description;
+        meetUp.eventTime = eventTime;
+        meetUp.tags = tags.map(tagName => {
+            const tag = new MeetUpTag();
+            tag.name = tagName;
+            return tag;
+        });
+        return meetUp;
+    }
+
+    static mapUpdateMeetUpDto = ({ title, description, eventTime, tags }: UpdateMeetUpDto): MeetUp => {
+        const meetUp = new MeetUp();
+        meetUp.title = title;
+        meetUp.description = description;
+        meetUp.eventTime = eventTime;
+        meetUp.tags = tags.map(tagName => {
             const tag = new MeetUpTag();
             tag.name = tagName;
             return tag;
         }) || [];
         return meetUp;
+    }
+
+    static mapToResponseMeetUpDto = ({ createdAt, createdBy, description, eventTime, title, tags }: MeetUp): ResponseMeetUpDto => {
+        const resp = new ResponseMeetUpDto();
+        resp.title = title;
+        resp.createdAt = createdAt;
+        resp.createdBy = createdBy.username;
+        resp.description = description;
+        resp.tags = tags.map(tag => tag.name);
+        resp.eventTime = eventTime;
+        return resp;
     }
 }
