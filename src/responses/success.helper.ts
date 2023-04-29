1ac1656cefeddd274e4f130152ba2aa3ef6363e8
Anton Rudkosvky <ranton497@gmail.com>
2023-04-29 13:34:34 +0300

    create functions to return common success

---
+++ src/responses/success.helper.ts
@@ -0,0 +1,15 @@
+import { SuccessResponse } from "./success.response";
+
+export const ok = (data: any): SuccessResponse => {
+    return {
+        successCode: 200,
+        data
+    }
+}
+
+export const created = (data: any): SuccessResponse => {
+    return {
+        successCode: 201,
+        data
+    }
+}
