3d4f25c10f7c9f289e504d954f5fa31ecd657903
Anton Rudkosvky <ranton497@gmail.com>
2023-04-29 13:36:47 +0300

    create update meet up dto

---
+++ src/meetups/dto/update.dto.ts
@@ -0,0 +1,25 @@
+import { IsDate, IsOptional, Length, MinDate, MinLength } from "class-validator";
+
+
+export class UpdateMeetUpDto {
+    @MinLength(10, {
+        message: 'Title should contain at least 10 symbols.'
+    })
+    @IsOptional()
+    title: string;
+
+    @MinLength(20, {
+        message: 'Description should contain at least 20 symbols.'
+    })
+    @IsOptional()
+    description: string;
+
+    @Length(3, 30, { each: true, message: 'Each tag should contains 3-30 sybmols.' })
+    @IsOptional()
+    tags: string[];
+
+    @IsDate()
+    @MinDate(new Date())
+    @IsOptional()
+    eventTime: Date;
+}
