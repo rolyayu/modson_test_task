8cd74a8142f206859ac8ea225a5a26f2ce5f4eba
Anton Rudkosvky <ranton497@gmail.com>
2023-04-29 13:45:16 +0300

    change responces flow add authorization

--- src/meetups/meetups.controller.ts
+++ src/meetups/meetups.controller.ts
@@ -1,26 +1,31 @@
-import { JsonController, Get, HttpCode, Body, Param, Res, Delete, Put, Patch, } from "routing-controllers";
-import CreateMeetUpDto from "./dto/create.dto";
-import MeetUp from "./meetups.entity";
+import { JsonController, Get, HttpCode, Body, Param, Res, Delete, Put, Patch, QueryParams, QueryParam, Authorized, CurrentUser, HttpError, } from "routing-controllers";
+import { CreateMeetUpDto } from "./dto/create.dto";
 import { MeetUpFactory } from "./meetups.factory";
-import { IBaseService } from "../interfaces";
 import { MeetUpDtoMapper } from "./dto/meetups.mapper";
 import { Response } from 'express';
-import { SuccessResponse, FailureResponse } from "../responses";
+import { SuccessResponse, FailureResponse, forbidden, notFound, ok, badRequest, created, internalError, } from "../responses";
+import { IMeetUpService } from "./meetups.service.interface";
+import MeetUp from "./meetups.entity";
+import { IsPositive } from "class-validator";
+import { UpdateMeetUpDto } from "./dto/update.dto";
+import { User, UserRole } from "../auth/users/users.entity";
 
 @JsonController('/api/meetups')
 export default class MeetupController {
-    private meetUpService: IBaseService<MeetUp>;
+    private meetUpService: IMeetUpService;
     constructor() {
         this.meetUpService = new MeetUpFactory().buildService();
     }
 
     @Get()
-    @HttpCode(200)
-    async getAll(): Promise<SuccessResponse> {
-        const successResp = new SuccessResponse();
-        successResp.successCode = 200;
-        successResp.data = await this.meetUpService.findAll();
-        return successResp;
+    async getAll(
+        @QueryParam('startPos', { required: false }) startPos: number = 0,
+        @QueryParam('pageSize', { required: false }) pageSize: number = 30
+    ): Promise<SuccessResponse> {
+        const meetUps = await this.meetUpService.findAll(startPos, pageSize);
+        console.log(meetUps);
+        const mappedMeetUps = meetUps.map(meet => MeetUpDtoMapper.mapToResponseMeetUpDto(meet));
+        return ok(mappedMeetUps);
     }
 
     @Get("/:id")
@@ -28,58 +33,70 @@ export default class MeetupController {
         const foundedMeetup = await this.meetUpService.findById(id);
         if (!foundedMeetup) {
             response.statusCode = 404;
-            const failResp = new FailureResponse();
-            failResp.errorCode = 404;
-            failResp.message = `Meet up with ${id} id doesn't exists.`
-            return failResp;
-        } else {
-            const successResp = new SuccessResponse();
-            successResp.successCode = 200;
-            successResp.data = foundedMeetup;
-            return successResp;
+            return notFound(`Meet up with ${id} id doesn't exists.`);
         }
+        return ok(MeetUpDtoMapper.mapToResponseMeetUpDto(foundedMeetup));
+
     }
 
     @Put()
     @HttpCode(201)
-    async createMeetUp(@Body() createDto: CreateMeetUpDto): Promise<SuccessResponse> {
-        const successResp = new SuccessResponse();
-        successResp.successCode = 201;
-        successResp.data = await this.meetUpService.save(MeetUpDtoMapper.mapCreateMeetUpDto(createDto));
-        return successResp;
+    @Authorized([UserRole.MANAGER])
+    async createMeetUp(
+        @Body({ validate: true }) createDto: CreateMeetUpDto,
+        @Res() res: Response,
+        @CurrentUser({ required: true }) user: User
+    ): Promise<SuccessResponse | FailureResponse> {
+        const mappedMeetUp = MeetUpDtoMapper.mapCreateMeetUpDto(createDto);
+        const savedMeetUp = await this.meetUpService.createMeetUp(mappedMeetUp, user);
+        return created(MeetUpDtoMapper.mapToResponseMeetUpDto(savedMeetUp));
     }
 
     @Delete("/:id")
-    async deleteMeetUpById(@Param("id") id: number, @Res() response: Response): Promise<SuccessResponse | FailureResponse> {
+    @Authorized([UserRole.MANAGER])
+    async deleteMeetUpById(
+        @Param("id") id: number,
+        @Res() response: Response,
+        @CurrentUser({ required: true }) user: User
+    ): Promise<SuccessResponse | FailureResponse> {
         try {
-            await this.meetUpService.deleteById(id);
-            const successResp = new SuccessResponse();
-            successResp.successCode = 200;
-            successResp.data = `Meet up with ${id} id successfully deleted.`;
-            return successResp;
+            await this.meetUpService.deleteByIdAccodingToUser(id, user);
+            return ok(`Meet up with ${id} successfully deleted`);
         } catch (e) {
-            const failResp = new FailureResponse();
-            failResp.errorCode = 400;
-            failResp.message = `Meet up with ${id} id doesn't exists.`;
-            return failResp;
+            if (e instanceof HttpError) {
+                response.statusCode = e.httpCode;
+                return {
+                    errorCode: e.httpCode,
+                    message: e.message,
+                    name: e.name
+                }
+            }
+            return internalError();
         }
     }
 
-    @Patch()
-    async updateMeetUp(@Body() meetUp: MeetUp, @Res() resp: Response): Promise<SuccessResponse | FailureResponse> {
-        if (!meetUp.id) {
-            resp.statusCode = 400;
-            const failResponse = new FailureResponse();
-            failResponse.errorCode = 400;
-            failResponse.message = `${meetUp} is not persisted. Can't update. If you want to persist meet up, try send Put request.`
-            return failResponse;
-        }
-        else {
-            resp.statusCode = 200;
-            const successResp = new SuccessResponse();
-            successResp.successCode = 200;
-            successResp.data = await this.meetUpService.save(meetUp);
-            return successResp;
+    @Patch("/:id")
+    @Authorized([UserRole.MANAGER])
+    async updateMeetUp(
+        @Body({ validate: true }) updateDto: UpdateMeetUpDto,
+        @Res() resp: Response,
+        @Param('id') id: number,
+        @CurrentUser({ required: true }) user: User
+    ): Promise<SuccessResponse | FailureResponse> {
+        try {
+            const withUpdatedProperties = MeetUpDtoMapper.mapUpdateMeetUpDto(updateDto);
+            const updatedMeetUp = await this.meetUpService.updateById(id, user, withUpdatedProperties);
+            return ok(MeetUpDtoMapper.mapToResponseMeetUpDto(updatedMeetUp));
+        } catch (e) {
+            if (e instanceof HttpError) {
+                resp.statusCode = e.httpCode;
+                return {
+                    errorCode: e.httpCode,
+                    message: e.message,
+                    name: e.name
+                }
+            }
+            return internalError();
         }
     }
 }
