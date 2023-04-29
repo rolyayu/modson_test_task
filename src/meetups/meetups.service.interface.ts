32379f07010d0c6f4c6a4d33f5c46971658edf4b
Anton Rudkosvky <ranton497@gmail.com>
2023-04-29 13:39:17 +0300

    add method to manipulate meetup acccording to user

--- src/meetups/meetups.service.interface.ts
+++ src/meetups/meetups.service.interface.ts
@@ -1,6 +1,9 @@
+import { User } from "../auth/users/users.entity";
 import { IBaseService } from "../interfaces";
 import MeetUp from "./meetups.entity";
 
 export interface IMeetUpService extends IBaseService<MeetUp> {
-
+    createMeetUp(meetUp: MeetUp, user: User): Promise<MeetUp>;
+    deleteByIdAccodingToUser(meetUpId: number, user: User): Promise<void>;
+    updateById(id: number, createdBy: User, withUpdatedProperties: MeetUp): Promise<MeetUp>
 }
