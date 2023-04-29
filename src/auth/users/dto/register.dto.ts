c510aa3a342f22d93cbac7642f5bbb5d03c0fa1d
Anton Rudkosvky <ranton497@gmail.com>
2023-04-29 13:47:40 +0300

    create all necessary dto for user manipulation

---
+++ src/auth/users/dto/register.dto.ts
@@ -0,0 +1,13 @@
+import { Length, Matches } from "class-validator";
+import { LoginUserDto } from "./login.dto";
+
+
+export class RegisterUserDto {
+    @Length(5, 40)
+    @Matches(new RegExp('[A-Za-z0-9_]*'))
+    username: string;
+
+    @Length(5, 40)
+    @Matches(new RegExp('[A-Za-z0-9_]*'))
+    password: string;
+}
---
+++ src/auth/users/dto/response.user.dto.ts
@@ -0,0 +1,7 @@
+import { User } from "../users.entity";
+
+export class ResponseUserDto {
+    id: number;
+    username: string;
+    role: string;
+}
