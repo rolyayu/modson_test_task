e9ce26dc8993890e046374707027e1b3f31847e9
Anton Rudkosvky <ranton497@gmail.com>
2023-05-05 20:52:47 +0300

    refactor: add errors names by default

--- src/responses/failure.helper.ts
+++ src/responses/failure.helper.ts
@@ -1,6 +1,6 @@
 import { FailureResponse } from "./failure.response";
 
-export const badRequest = (message: string, name?: string): FailureResponse => {
+export const badRequest = (message: string, name: string = 'Bad Request'): FailureResponse => {
     return {
         errorCode: 400,
         message,
@@ -8,7 +8,7 @@ export const badRequest = (message: string, name?: string): FailureResponse => {
     }
 }
 
-export const unauthorized = (message: string, name?: string): FailureResponse => {
+export const unauthorized = (message: string, name = 'Unathorized'): FailureResponse => {
     return {
         errorCode: 401,
         message,
@@ -16,14 +16,14 @@ export const unauthorized = (message: string, name?: string): FailureResponse =>
     }
 }
 
-export const forbidden = (message: string, name?: string): FailureResponse => {
+export const forbidden = (message: string, name: string = 'Forbidden'): FailureResponse => {
     return {
         errorCode: 403,
         message,
         name
     }
 }
-export const notFound = (message: string, name?: string): FailureResponse => {
+export const notFound = (message: string, name: string = 'Not Found'): FailureResponse => {
     return {
         errorCode: 404,
         message,
