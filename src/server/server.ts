9ea28d268c13c07fc84e8b8698ce0947534d2eef
Anton Rudkosvky <ranton497@gmail.com>
2023-05-05 20:39:00 +0300

    refactor<server>: change approach
    Instead of createExpressServer use useExrpessServer
    Extract routing-controllers server params into separate function

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,5 @@
-import { Express, Request } from 'express';
-import { Action, createExpressServer } from 'routing-controllers';
+import express, { Express, Request } from 'express';
+import { Action, RoutingControllersOptions, useExpressServer } from 'routing-controllers';
 import { CurrentUserChecker } from 'routing-controllers/types/CurrentUserChecker';
 import { AuthorizationChecker } from 'routing-controllers/types/AuthorizationChecker';
 
@@ -8,6 +8,8 @@ import { JwtService } from '../auth';
 import { AuthError } from '../errors';
 
 import { UserFactory, IUserService, User } from '../users';
+import { GlobalErrorHanlde } from '../middlewares';
+import cookieParser from 'cookie-parser';
 
 export class ExpressServer {
     private static server: Express;
@@ -17,14 +19,22 @@ export class ExpressServer {
 
     static getServer = (): Express => {
         if (!this.server) {
-            this.server = createExpressServer({
-                controllers: [__dirname + '/../**/*.controller.ts'],
-                authorizationChecker: this.authChecker(),
-                currentUserChecker: this.currentUserChecker(),
-                validation: true
-            });
+            const expressServer: Express = express();
+            expressServer.use(cookieParser())
+            this.server = useExpressServer(expressServer, this.getRoutingControllersParams())
         }
         return this.server;
+    }
+
+    private static getRoutingControllersParams = (): RoutingControllersOptions => {
+        return {
+            controllers: [__dirname + '/../**/*.controller.ts'],
+            middlewares: [GlobalErrorHanlde],
+            authorizationChecker: this.authChecker(),
+            currentUserChecker: this.currentUserChecker(),
+            validation: true,
+            defaultErrorHandler: false,
+        }
     }
 
     private static currentUserChecker = (): CurrentUserChecker => {
@@ -32,12 +42,12 @@ export class ExpressServer {
             const req: Request = action.request;
             const authHeader = req.header('authorization');
             if (!authHeader) {
-                throw new AuthError(402, 'Authorize to make this request.')
+                throw new AuthError('Authorize to make this request.')
             }
             const accessToken = JwtService.extractTokenFromHeader(authHeader);
             const payload = JwtService.extractAccessPayload(accessToken);
             if (!JwtService.isAccess(payload)) {
-                throw new AuthError(400, 'Given token is not access.');
+                throw new AuthError('Given token is not access.');
             }
             return await this.userService.findByUsername(payload.username);
         }
@@ -47,31 +57,26 @@ export class ExpressServer {
         return async (action: Action, roles: string[]): Promise<boolean> => {
             const req: Request = action.request;
             const authHeader = req.header('Authorization');
-            if (!authHeader) {
+            const token = JwtService.extractTokenFromHeader(authHeader!);
+            console.log(token);
+            const payload = JwtService.extractAccessPayload(token);
+            if (!JwtService.isAccess(payload)) {
                 return false;
             }
-            try {
-                const token = JwtService.extractTokenFromHeader(authHeader);
-                console.log(token);
-                const payload = JwtService.extractAccessPayload(token);
-                if (!JwtService.isAccess(payload)) {
-                    return false;
-                }
-                const user = await this.userService.findByUsername(payload.username);
-                if (!user) {
-                    return false;
-                }
-                if (!roles) {
-                    return true;
-                }
-                if (!roles.find(role => role == user.role.toString())) {
-                    return false;
-                }
+            const user = await this.userService.findByUsername(payload.username);
+            if (!user) {
+                return false;
+            }
+            if (!roles) {
                 return true;
-            } catch (e) {
-                console.log(e)
+            }
+            if (!roles.find(role => role == user!.role.toString())) {
                 return false;
             }
+            return true;
+
         }
     }
+
+
 }
