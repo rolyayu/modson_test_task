ed9877d3bbf0cab32e0a48ca0bcb286455add19e
Anton Rudkosvky <ranton497@gmail.com>
2023-05-05 20:27:16 +0300

    feat: create file with all constants

---
+++ src/shared/constants.ts
@@ -0,0 +1,7 @@
+
+export const constants = {
+    refreshTokenExpiration: '30 days',
+    accessTokenExpiration: '15 mins',
+    refreshCookieMaxAge: 1000 * 60 * 60 * 24 * 30,
+    accessCookieMaxAge: 1000 * 60 * 15
+} as const;
